9d699dac97bdc8b15aabc3a0b4fb6498c57771e3
njleonzhang <scream3616@sina.com>
2018-03-06 17:02:34 +0800

    module and namespace study

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import callPeople from './callPeople'
 import { cons } from './j'
 import * as frameworks from './frameworks/index'
 import { React, Vue } from './frameworks/index'
+import { DataTablesServer } from 'vue-data-tables'
 
 var leon = {
   firstName: 'zhang'
@@ -9,10 +10,15 @@ var leon = {
 
 callPeople(leon)
 
-// Space来自于全局的namespace
-console.log(Space.x, Space.y, Space.z, frameworks)
+try {
+  console.log(Space.x, Space.y, Space.z, frameworks)
+} catch (e) {
+  console.log(e)
+}
 
 new React()
 new Vue()
+
+console.log(DataTablesServer)
 
 export default 1 > 0
--- src/space.ts
+++ src/space.ts
@@ -1,4 +1,10 @@
+// 跨文件的namespace在webpack里面是不能用的。
+// https://github.com/TypeStrong/ts-loader/issues/193
 namespace Space {
   export let x = 1
   export let y = '2'
+
+  export interface T {
+
+  }
 }
