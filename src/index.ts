1d5a4232b848de9c1fed27a190fe968dc8f205c8
njleonzhang <scream3616@sina.com>
2018-03-07 20:39:13 +0800

    添加注释

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import callPeople from './callPeople'
-import { cons } from './j'
+import { cons } from './j' // 引用js
 import * as frameworks from './frameworks/index'
 import { React, Vue } from './frameworks/index'
-import { DataTablesServer } from 'vue-data-tables'
+import { DataTablesServer } from 'vue-data-tables' // 为vue-data-tables写了一个d.ts声明文件，否则此处无法导入
 
 var leon = {
   firstName: 'zhang'
@@ -13,6 +13,8 @@ callPeople(leon)
 console.log(cons)
 
 try {
+  // 跨文件的namespace在webpack里面是不能用的。
+  // https://github.com/TypeStrong/ts-loader/issues/193
   console.log(Space.x, Space.y, Space.z, frameworks)
 } catch (e) {
   console.log(e)
--- src/module.ts
+++ src/module.ts
@@ -1,3 +1,5 @@
+// typescript里有export或import的文件即是模块
+
 export class Animal {
 
 }
